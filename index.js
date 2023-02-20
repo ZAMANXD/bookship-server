b629c54fc18adf8aa4e5ee7ba1b004cebbbd0a06
Achintya151 <achintyakumartalukdar@gmail.com>
2023-02-20 13:54:34 +0600

    issue created

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+// In-memory data store for cart items
+const cartItems = [];
+
+
 // SendGrid config
 // const sgMail = require('@sendgrid/mail');
 // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -57,6 +61,7 @@ async function run() {
       .db('bookship')
       .collection('publications');
     const subscriberCollection = client.db('bookship').collection('subscriber');
+    const cartCollection = client.db('bookship').collection('cart');
 
     app.post('/create-payment-intent', async (req, res) => {
       const order = req.body;
@@ -607,6 +612,84 @@ async function run() {
         .toArray();
       res.send(result);
     });
+    // Cart Items
+   // Get the current contents of the cart
+// app.get('/cart', async (req, res) => {
+//   const query = {};
+//   const result= await cartCollection.find(query).toArray();
+//   res.send(result);
+// });
+// get cart for user
+app.get('/cart/:userEmail', async (req, res) => {
+  const userEmail = req.params.userEmail;
+  const query = { userEmail };
+  const result = await cartCollection.findOne(query);
+  res.send(result);
+});
+
+// add to cart
+app.post('/add-to-cart', async (req, res) => {
+  const { id, quantity, userEmail } = req.body;
+  let cart;
+  cart = await cartCollection.findOne({ userEmail });
+  if (!cart) {
+    await cartCollection.insertOne({ userEmail, items: [{ id, quantity }] });
+  } else {
+    const existingItem = cart.items.find(item => item.id === id);
+
+    if (existingItem) {
+      existingItem.quantity += quantity;
+    } else {
+      cart.items.push({ id, quantity })
+    }
+    await cartCollection.updateOne({ _id: cart._id }, { $set: { items: cart.items } });
+  }
+  res.sendStatus(200);
+});
+
+// subtract from cart
+app.put('/subtract-from-cart', async (req, res) => {
+  const { id, quantity, userEmail } = req.body;
+  let cart;
+  cart = await cartCollection.findOne({ userEmail });
+
+  if (!cart) {
+    res.sendStatus(404);
+  } else {
+    const existingItem = cart.items.find(item => item.id === id);
+
+    if (existingItem) {
+      existingItem.quantity -= quantity;
+
+      if (existingItem.quantity <= 0) {
+        cart.items = cart.items.filter(item => item.id !== id);
+      }
+
+      await cartCollection.updateOne({ _id: cart._id }, { $set: { items: cart.items } });
+
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+  }
+});
+
+// remove from cart
+app.delete('/remove-from-cart/:id/:userEmail', async (req, res) => {
+  const { id, userEmail } = req.params;
+  let cart;
+  cart = await cartCollection.findOne({ userEmail });
+  if (!cart) {
+    res.sendStatus(404);
+  } else {
+    cart.items = cart.items.filter(item => item.id !== parseInt(id));
+
+    await cartCollection.updateOne({ _id: cart._id }, { $set: { items: cart.items } });
+
+    res.sendStatus(200);
+  }
+});
+
   } finally {
   }
 }
